0944a3862b175a985dda4a0e5b22fbd63b282140
Niktia <MrrockyrevardPS4@yandex.ru>
2023-08-09 14:44:20 +0300

    Finished (no comments)

--- app/src/main/java/com/example/pwgen/generate.kt
+++ app/src/main/java/com/example/pwgen/generate.kt
@@ -33,45 +33,8 @@ class Generate {
             a = false
         }
         return a
-
-//        while (plus < const.masiv.size) {
-//            Log.d("Проврека плюса", plus.toString())
-//            Log.d("Проврека размера массива", const.masiv.size.toString())
-//            for (lettersInMasiv in const.masiv[plus]) {
-//                for (simvol in string) {
-//                    if (simvol == lettersInMasiv) {
-//                        provero4nieMasiv.add(true)
-//                    }
-//                }
-//            }
-//            plus++
-//        }
-//
-//        for (element in provero4nieMasiv) {
-//            Log.d("Элементы в масиве для проверки", element.toString())
-//            if (!element) {
-//                Log.d("Должно быть возвращение", "Возвращение")
-//                return prov(string)
-//            }
-//        }
-
-        /*for (ElementsMasiva in const.masiv) {
-            for (lettersInMasiv in ElementsMasiva) {
-                for (simvol in string) {
-                    if (lettersInMasiv == simvol) {
-                        a = true
-                        //Как проверить все строки?
-                    }
-                }
-            }
-        }
-
-        return a*/
-
     }
 
-
-    // по идеи правильная функция
     fun AddAndProvPassword(quantity:Int, longPassword: Int) {
 
         while (a < quantity) {
@@ -96,44 +59,10 @@ class Generate {
         string = ""
         const.simvols = ""
         const.masiv.clear()
-
-//        while (a < quantity) {
-//                while (b < longPassword) {
-//                    val rand = const.simvols.random().toChar()
-//                    string += rand
-//                    b++
-//                }
-//
-//            if (!prov(const.simvols,string)) {
-//                if (a == 0) {
-//                    a = 0
-//                    string = ""
-//                    b = 0
-//                }
-//                else {
-//                    a--
-//                    string = ""
-//                    b = 0
-//                }
-//
-//            }
-//            else {
-//                const.passwords.add(string)
-//                b = 0
-//                string = ""
-//                a++
-//            }
-//        }
-//        a = 0
-//        b = 0
-//        string = ""
-//        const.simvols = ""
     }
 
 
     fun gen(longPassword: Int, chk1:CheckBox, chk2:CheckBox, chk3:CheckBox, chk4:CheckBox) {
-
-
         if (chk1.isChecked && chk2.isChecked && chk3.isChecked && chk4.isChecked) {
             const.masiv.add(const.letter)
             const.masiv.add(const.letterPlus)
@@ -225,40 +154,6 @@ class Generate {
             const.masiv.add(const.letter)
             const.simvols += const.letter
             AddAndProvPassword(5,longPassword)
-//            while (a < 5) {
-//                while (b < longPassword) {
-//                    val rand = const.letter.random()
-//                    string += rand
-//                    b++
-//                }
-//                const.passwords.add(string)
-//                b = 0
-//                string = ""
-//                a++
-//            }
-//            a = 0
-//            b = 0
-//            string = ""
         }
-
-
-
-
-//        if (chk1.isChecked && chk2.isChecked) {
-//            masiv.add(const.letter)
-//            masiv.add(const.letterPlus)
-//
-//            while (a < 5) {
-//                while (b < longPassword) {
-//                    val rand = masiv.random()
-//                    string += rand
-//                    b++
-//                }
-//                a++
-//            }
-//
-//
-//        }
-
     }
 }
